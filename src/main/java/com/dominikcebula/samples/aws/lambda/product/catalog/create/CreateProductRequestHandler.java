d24908f8559ac456c234f5d7c06eb717f9d2be00
Dominik Cebula <dominikcebula@gmail.com>
2025-03-30 18:05:32 +0200

    aws-samples-lambda-serverless-framework-java21

---
+++ src/main/java/com/dominikcebula/samples/aws/lambda/product/catalog/create/CreateProductRequestHandler.java
@@ -0,0 +1,18 @@
+package com.dominikcebula.samples.aws.lambda.product.catalog.create;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class CreateProductRequestHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
+        var response = new APIGatewayProxyResponseEvent();
+
+        response.setStatusCode(200);
+        response.setBody("OK");
+
+        return response;
+    }
+}
